427eca83db2060bf695e071f660376cf4f7961df
Hannah Sison <hannahsison18@gmail.com>
2015-03-27 16:26:40 -0700

    added basic crud routes for volunteer. added edit and show capabilities

--- volunteerMe/app/controllers/volunteers_controller.rb
+++ volunteerMe/app/controllers/volunteers_controller.rb
@@ -8,4 +8,17 @@ class VolunteersController < ApplicationController
     @volunteer = Volunteer.find(params[:id])
   end
 
+  def update
+    @volunteer.update_attributes(user_params)
+
+    redirect_to @volunteer
+  end
+
+  def destroy
+    @volunteer = Volunteer.find(params[:id])
+    @volunteer.destroy
+
+    redirect_to volunteers_path
+  end
+
 end
