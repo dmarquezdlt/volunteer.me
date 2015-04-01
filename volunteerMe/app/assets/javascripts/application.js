50b6e89df648ceacc7aac9e849a9ac29ad656cd8
Hannah Sison <hannahsison18@gmail.com>
2015-04-01 11:03:03 -0700

    popup modal for volunteer sign in

--- volunteerMe/app/assets/javascripts/application.js
+++ volunteerMe/app/assets/javascripts/application.js
@@ -34,4 +34,27 @@ $(document).ready(function(){
 
   var matchlist = new List('matches-list', options);
 
+  $("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
+
+      // Calling Login Form
+      // $("#login_form").click(function () {
+      //     $(".social_login").hide();
+      //     $(".user_login").show();
+      //     return false;
+      // });
+
+      // // Calling Register Form
+      // $("#register_form").click(function () {
+      //     $(".social_login").hide();
+      //     $(".user_register").show();
+      //     $(".header_title").text('Register');
+      //     return false;
+      // });
+
+
+
+
+
 });
+
+
--- volunteerMe/app/models/volunteer.rb
+++ volunteerMe/app/models/volunteer.rb
@@ -13,6 +13,6 @@ class Volunteer < ActiveRecord::Base
   has_many :superpowers
 
   ## For uploading pics
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://findinit.files.wordpress.com/2015/03/volunteer_hero.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://findinit.files.wordpress.com/2015/03/volunteer_hero.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
